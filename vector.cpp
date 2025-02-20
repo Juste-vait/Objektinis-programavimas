8ff0c96dc7c1b5b69a0a8cd94791fd2fcfec3bab
Juste Vaitkeviciute <juste@Justes-MacBook-Air.local>
2025-02-20 11:17:40 +0100

    Implementuotas meniu

--- vector.cpp
+++ vector.cpp
@@ -4,7 +4,10 @@
 #include <algorithm>
 #include <limits>
 #include <string>
-#include "funkcijos.h"
+#include <random>
+#include <cstdlib>
+#include <ctime>
+
 
 using namespace std;
 
@@ -16,13 +19,106 @@ struct Studentas {
     double galutinis;
 };
 
+double skaiciuotiVidurki(const vector<int>& pazymiai) {
+    if (pazymiai.empty()) return 0.0;
+    int suma = 0;
+    for (int paz : pazymiai) suma += paz;
+    return static_cast<double>(suma) / pazymiai.size();
+}
 
-int main() {
-    vector<Studentas> studentai;
-    char pasirinkimas;
+double skaiciuotiMediana(vector<int>& pazymiai) {
+    if (pazymiai.empty()) return 0.0;
+    sort(pazymiai.begin(), pazymiai.end());
+    size_t n = pazymiai.size();
+    if (n % 2 == 0) {
+        return (pazymiai[n / 2 - 1] + pazymiai[n / 2]) / 2.0;
+    } else {
+        return pazymiai[n / 2];
+    }
+}
 
-    cout << "Pasirinkite galutinio balo skaiciavimą (V - vidurkis, M - mediana): ";
-    cin >> pasirinkimas;
+double skaiciuotiGalutiniVidurkis(const vector<int>& namuDarbai, int egzaminas) {
+    double vidurkis = skaiciuotiVidurki(namuDarbai);
+    return 0.4 * vidurkis + 0.6 * static_cast<double>(egzaminas);
+}
+
+double skaiciuotiGalutiniMediana(vector<int>& namuDarbai, int egzaminas) {
+    double mediana = skaiciuotiMediana(namuDarbai);
+    return 0.4 * mediana + 0.6 * static_cast<double>(egzaminas);
+}
+
+void ivestiStudenta(vector<Studentas>& studentai, char pasirinkimas) {
+    Studentas stud;
+    while (true) {  
+    cout << "Ar norite pridėti studentą? (T/N): ";
+    char tesiame;
+    cin >> tesiame;
+    if (tesiame == 'N' || tesiame == 'n') break;
+
+    Studentas stud;
+    cout << "\nĮveskite studento vardą: ";
+    cin >> stud.vardas;
+    cout << "Įveskite studento pavardę: ";
+    cin >> stud.pavarde;
+    
+    cout << "Įveskite namų darbų rezultatus (baigti -1):\n";
+    while (true) {
+            int rezultatas;
+            if (!(cin >> rezultatas)) {
+            cout << "Klaida! Įveskite tik skaičius.\n";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            continue;
+            }
+        if (rezultatas == -1) break;
+        stud.namuDarbai.push_back(rezultatas);
+        }
+        
+        cout << "Įveskite egzamino rezultatą: ";
+        while (!(cin >> stud.egzaminas)) {
+        cout << "Klaida! Įveskite tik skaičius.\n";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        if (pasirinkimas == 'V' || pasirinkimas == 'v') {
+        double vidurkis = skaiciuotiVidurki(stud.namuDarbai);
+        stud.galutinis = 0.4 * vidurkis + 0.6 * stud.egzaminas;
+        } else {
+        double mediana = skaiciuotiMediana(stud.namuDarbai);
+        stud.galutinis = 0.4 * mediana + 0.6 * stud.egzaminas;
+        }
+    studentai.push_back(stud);
+    }
+    
+}
+
+void generuotiPazymius(Studentas& stud, char pasirinkimas) {
+    
+    int kiekis = rand() % 5 + 3;  
+
+    
+    for (int i = 0; i < kiekis; ++i) {
+        stud.namuDarbai.push_back(rand() % 10 + 1);  
+    }
+
+    stud.egzaminas = rand() % 10 + 1; 
+    
+    if (pasirinkimas == 'V' || pasirinkimas == 'v') {
+        double vidurkis = skaiciuotiVidurki(stud.namuDarbai);
+        stud.galutinis = 0.4 * vidurkis + 0.6 * stud.egzaminas;
+    } else {
+        double mediana = skaiciuotiMediana(stud.namuDarbai);
+        stud.galutinis = 0.4 * mediana + 0.6 * stud.egzaminas;
+    }
+
+    
+}
+
+void generuotiStudentus(vector<Studentas>& studentai, char pasirinkimas) {
+    vector<string> vardai = {"Jonas", "Petras", "Antanas", "Nojus", "Lukas"};
+    vector<string> pavardes = {"Kazlauskas", "Petraitis", "Jonaitis", "Biliūnas", "Lukauskis"};
 
     while (true) {
         cout << "Ar norite pridėti studentą? (T/N): ";
@@ -31,35 +127,9 @@ int main() {
         if (tesiame == 'N' || tesiame == 'n') break;
 
         Studentas stud;
-        cout << "\nĮveskite studento vardą: ";
-        cin >> stud.vardas;
-        cout << "Įveskite studento pavardę: ";
-        cin >> stud.pavarde;
-
-        cout << "Įveskite namų darbų rezultatus (baigti -1):\n";
-        while (true) {
-            int rezultatas;
-            if (!(cin >> rezultatas)) { 
-                cout << "Klaida! Įveskite tik skaičius.\n";
-                cin.clear();
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                continue;
-            }
-            if (rezultatas == -1) break;
-            stud.namuDarbai.push_back(rezultatas);
-        }
-
-        cout << "Įveskite egzamino rezultatą: ";
-        while (true) {
-            if (!(cin >> stud.egzaminas)) { 
-                cout << "Klaida! Įveskite tik skaičių.\n";
-                cin.clear();
-                cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                continue;
-            }
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
-            break;
-        }
+        stud.vardas = vardai[rand() % vardai.size()];
+        stud.pavarde = pavardes[rand() % pavardes.size()];
+        generuotiPazymius(stud, pasirinkimas);
 
         if (pasirinkimas == 'V' || pasirinkimas == 'v') {
             double vidurkis = skaiciuotiVidurki(stud.namuDarbai);
@@ -70,6 +140,52 @@ int main() {
         }
 
         studentai.push_back(stud);
+    }
+}
+
+
+int main() {
+    srand(time(0));  
+    vector<Studentas> studentai;
+    char pasirinkimas;
+    int pasirinkimasMeniu;
+
+    cout << "Pasirinkite galutinio balo skaičiavimą (V - vidurkis, M - mediana): ";
+    cin >> pasirinkimas;
+    
+    while (true) {
+        cout << "\nPasirinkite veiksmą:\n";
+        cout << "1 - Rankiniu būdu įvesti duomenis\n";
+        cout << "2 - Generuoti pažymius\n";
+        cout << "3 - Generuoti ir pažymius, ir vardus/pavardes\n";
+        cout << "4 - Baigti darbą\n";
+        cout << "Jūsų pasirinkimas: ";
+        cin >> pasirinkimasMeniu;
+
+        if (pasirinkimasMeniu == 4) {
+            break;
+        } else if (pasirinkimasMeniu == 1) {
+            ivestiStudenta(studentai, pasirinkimas);
+        } else if (pasirinkimasMeniu == 2) {
+            while (true) {  
+                cout << "Ar norite pridėti studentą? (T/N): ";
+                char tesiame;
+                cin >> tesiame;
+                if (tesiame == 'N' || tesiame == 'n') break;
+
+                Studentas stud;
+                cout << "\nĮveskite studento vardą: ";
+                cin >> stud.vardas;
+                cout << "Įveskite studento pavardę: ";
+                cin >> stud.pavarde;
+                generuotiPazymius(stud, pasirinkimas);
+                studentai.push_back(stud);
+            }
+        } else if (pasirinkimasMeniu == 3) {
+            generuotiStudentus(studentai, pasirinkimas);
+        } else {
+            cout << "Neteisingas pasirinkimas! Bandykite dar kartą.\n";
+        }
     }
 
     cout << "\n---------------------------------------------------\n";
