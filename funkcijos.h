8118e211b3bc5673ca04ce67ddaa82540b5d5bf2
Juste Vaitkeviciute <juste@Justes-MacBook-Air.local>
2025-03-19 00:27:06 +0200

    .

--- funkcijos.h
+++ funkcijos.h
@@ -13,6 +13,8 @@
 #include <fstream>
 #include <sstream>
 #include <chrono>
+#include <list>
+#include <deque>
 #include <variant>
 #include <struct.h>
 
@@ -29,20 +31,21 @@ struct Studentas {
     double galutinisMed;
 };
 
-using StudentaiVariant = variant<vector<Studentas>, list<Studentas>, deque<Studentas>>;
-void pasirinktiKonteineri(StudentaiVariant &studentai, char pasirinkimas)
+using studentaiVariant = variant<vector<Studentas>, list<Studentas>, deque<Studentas>>;
+void pasirinktiKonteineri(studentaiVariant &studentai, char pasirinkimasK);
 double skaiciuotiVidurki(const vector<int>& pazymiai);
 double skaiciuotiMediana(vector<int>& pazymiai);
 void ivestiStudenta(vector<Studentas>& studentai, char pasirinkimas);
 void generuotiPazymius(Studentas& stud, char pasirinkimas);
 void generuotiStudentus(vector<Studentas>& studentai, char pasirinkimas);
-void nuskaitytiIsFailo(vector<Studentas>& studentai);
-void rusiuotiStudentus(vector<Studentas>& studentai);
+void nuskaitytiIsFailo(studentaiVariant &studentai);
+void rusiuotiStudentus(studentaiVariant &studentai);
 void isvestiDuomenis(vector<Studentas>& studentai);
 void generuotiFailus(vector<int>& dydziai);
-void grupuotiStudentus(vector<Studentas>& studentai, vector<Studentas>& kietekai, vector<Studentas>& nuskriaustukai);
+void grupuotiStudentus(studentaiVariant &studentai, studentaiVariant& kietekai, studentaiVariant& nuskriaustukai);
 void isvestiIDuFailus(vector<Studentas>& kietekai, vector<Studentas>& nuskriaustukai);
 void testavimoFunkcija_1(vector<int> dydziai);
 void testavimoFunkcija_2(vector<Studentas>& studentai, vector<Studentas>& kietekai, vector<Studentas>& nuskriaustukai);
+void testavimoFunkcija_3(studentaiVariant &studentai, studentaiVariant &kietekai, studentaiVariant &nuskriaustukai);
 
 #endif
